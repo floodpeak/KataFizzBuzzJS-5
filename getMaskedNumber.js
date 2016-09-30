5cfb64140de999ed46196c83eec35d3206af7473
floodpeak <hongfengzhou@yahoo.com>
2016-10-01 00:06:04 +0800

    success for 3 5 6

--- getMaskedNumber.js
+++ getMaskedNumber.js
@@ -1,3 +1,9 @@
 export default (number)=>{
+  if(number % 3 == 0){
+    return 'Fizz'
+  }
+  if(number == 5){
+    return 'Buzz'
+  }
   return number
 }
--- test/getMaskedNumber.test.js
+++ test/getMaskedNumber.test.js
@@ -8,4 +8,13 @@ describe('test env',function(){
   it('should be 2 for 2',function(){
     expect(getMaskedNumber(2)).to.be.equal(2)
   })
+  it('should be Fizz for 3',function(){
+    expect(getMaskedNumber(3)).to.be.equal('Fizz')
+  })
+  it('should be Buzz for 5',function(){
+    expect(getMaskedNumber(5)).to.be.equal('Buzz')
+  })
+  it('should be Fizz for 6',function(){
+    expect(getMaskedNumber(6)).to.be.equal('Fizz')
+  })
 })
